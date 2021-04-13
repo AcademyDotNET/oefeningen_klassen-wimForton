838e6970d387baeb2b706200bb7db55aaa42e37a
wim forton <wim.forton@gmail.com>
2021-04-13 13:20:35 +0200

    classes RapportModule, Nummers, Student, Pizza, Basketballer

--- Classes/Class1.cs
+++ Classes/Class1.cs
@@ -4,6 +4,8 @@ using System.Text;
 
 namespace Classes
 {
+    public enum Klassen { EA1, EA2, EA3, EB1, EB2 }
+    public enum basketbalNiveaus { amateurs, derde, tweede, eerste }
     class RapportModule
     {
         public double percentage = 0;
@@ -33,8 +35,8 @@ namespace Classes
     class Nummers
     {
 
-        public double Getal1;
-        public double Getal2;
+        public double Getal1 { get; set; } = 0;
+        public double Getal2 { get; set; } = 0;
         public double Som()
         {
             return Getal1 + Getal2;
@@ -50,6 +52,146 @@ namespace Classes
         public double Quotient()
         {
             return Getal1 / Getal2;
+        }
+    }
+    class Student {
+        private string naam;
+        private int leeftijd;
+        public Klassen WelkeKlas { get; set; }
+        private double puntenCommunicatie;
+        private double puntenProgrammingPrinciples;
+        private double puntenWebTech;
+        public string Naam
+        {
+            set
+            {
+                naam = value;
+            }
+        }
+        public int Leeftijd {
+            set {
+                leeftijd = value;
+            }
+        }
+        public double PuntenCommunicatie
+        {
+            set
+            {
+                puntenCommunicatie = value;
+            }
+        }
+        public double PuntenProgrammingPrinciples
+        {
+            set
+            {
+                puntenProgrammingPrinciples = value;
+            }
+        }
+        public double PuntenWebTech
+        {
+            set
+            {
+                puntenWebTech = value;
+            }
+        }
+        public void GeefOverzicht()
+        {/*
+                    private string naam;
+        private int leeftijd;
+        public Klassen WelkeKlas { get; set; }
+        private double puntenCommunicatie;
+        private double puntenProgrammingPrinciples;
+        private double puntenWebTech;*/
+        Console.WriteLine($"naam: {naam}\nleeftijd: {leeftijd}\nklas: {WelkeKlas}\npuntenCommunicatie: {puntenCommunicatie}\npuntenProgrammingPrinciples: {puntenProgrammingPrinciples}\npuntenWebTech: {puntenWebTech}\n");
+        }
+    }
+    class Pizza
+    {
+        private string[] toppingsArray = {"ananas", "banaan", "citroen", "margarita" };
+        private string toppings = "margarita";
+        private int diameter = 15;
+        private double price = 5;
+        public string Toppings
+        {
+            get
+            {
+                return toppings;
+            }
+            set
+            {
+                toppings = value;
+                if (Array.IndexOf(toppingsArray, toppings) == -1)
+                {
+                    toppings = "margarita";
+                }
+                
+            }
+        }
+        public int Diameter
+        {
+            get
+            {
+                return diameter;
+            }
+            set 
+            {
+                diameter = value;
+                if (diameter < 15) { diameter = 15; }
+                if (diameter > 40) { diameter = 40; }
+            }
+        }
+        public double Price
+        {
+            get {
+                return price;
+            }
+            set
+            {
+                price = value;
+                if (price < 5) { price = 5; }
+            }
+        }
+    }
+    class Basketballer
+    {
+        private string naam = "";
+        private int rugNummer = 0;
+        private bool doelMan = false;
+        private bool reserve = false;
+        private basketbalNiveaus niveau = basketbalNiveaus.amateurs;
+
+        public void StelIn(string inNaam, int inRugNummer, bool inDoelman, bool inReserve, basketbalNiveaus inNiveau)
+        {
+            naam = inNaam;
+            rugNummer = inRugNummer;
+            doelMan = inDoelman;
+            reserve = inReserve;
+            niveau = inNiveau;
+        }
+        public void StelUEensVoor() {
+            string stelVoorDoelman =  $"ik ben {(doelMan ? "een": "geen")} doelman";
+            string stelVoorReserve = $"ik ben {(reserve ? "een" : "geen")} reserve";
+            Console.WriteLine($"ik ben {naam}, mijn rugnummer is {rugNummer}, {stelVoorDoelman}, {stelVoorReserve}, ik speel in {niveau}");
+        }
+
+        public void Shot()
+        {
+            Random shotrandom = new Random();
+            int shotResult = shotrandom.Next(0, 3);
+            switch(shotResult)
+            {
+                case 0:
+                    Console.WriteLine($"{naam} gooit er naast");
+                    break;
+                case 1:
+                    Console.WriteLine($"{naam} gooit op de ring");
+                    break;
+                case 2:
+                    Console.WriteLine($"{naam} maakt een goal!!!");
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
--- Classes/ClassesProgram.cs
+++ Classes/ClassesProgram.cs
@@ -4,12 +4,17 @@ namespace Classes
 {
     class ClassesProgram
     {
+        
+        
+
         static void Main(string[] args)
         {
+            /*
             RapportModule MyRapportModule = new RapportModule();
             MyRapportModule.percentage = 65;
             MyRapportModule.PrintGraad();
-
+            */
+            /*
             Nummers paar1 = new Nummers();
             paar1.Getal1 = 12;
             paar1.Getal2 = 34;
@@ -19,6 +24,39 @@ namespace Classes
             Console.WriteLine("Verschil = " + paar1.Verschil());
             Console.WriteLine("Product = " + paar1.Product());
             Console.WriteLine("Quotient = " + paar1.Quotient());
+            */
+            /*
+            Student student1 = new Student();
+            student1.WelkeKlas = Klassen.EA2;
+            student1.Leeftijd = 21;
+            student1.Naam = "Joske Vermeulen";
+            student1.PuntenCommunicatie = 12;
+            student1.PuntenProgrammingPrinciples = 15;
+            student1.PuntenWebTech = 13;
+
+            student1.GeefOverzicht();
+            */
+            /*
+            Pizza pizza1 = new Pizza();
+            Console.WriteLine(pizza1.Diameter);
+            pizza1.Toppings = "appel";
+            Console.WriteLine(pizza1.Toppings);
+            pizza1.Price = 20;
+            Console.WriteLine(pizza1.Price);
+            */
+            /*
+            Basketballer basketBaller1 = new Basketballer();
+            
+            basketBaller1.StelIn("Tim", 5, false, true, basketbalNiveaus.tweede);
+            basketBaller1.StelUEensVoor();
+            basketBaller1.Shot();
+            basketBaller1.Shot();
+            basketBaller1.Shot();
+            basketBaller1.Shot();
+            */
+
+
+
         }
     }
 }
