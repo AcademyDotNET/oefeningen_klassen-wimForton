0b8683a7a3b79b2e881217466fa08bc68bc92205
wim forton <wim.forton@gmail.com>
2021-04-13 09:37:09 +0200

    Classes Rapport and Nummers added

---
+++ Classes/Class1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes
+{
+    class RapportModule
+    {
+        public double percentage = 0;
+        public void PrintGraad()
+        {
+            if (percentage < 50 && percentage >= 0) { 
+                Console.WriteLine("Niet geslaagd"); 
+            }
+            else if (percentage < 68)
+            {
+                Console.WriteLine("voldoende");
+            }
+            else if (percentage < 75)
+            {
+                Console.WriteLine("onderscheiding");
+            }
+            else if (percentage < 85)
+            {
+                Console.WriteLine("grote onderscheiding");
+            }
+            else if (percentage < 100)
+            {
+                Console.WriteLine("grootste onderscheiding");
+            }
+        }
+    }
+    class Nummers
+    {
+
+        public double Getal1;
+        public double Getal2;
+        public double Som()
+        {
+            return Getal1 + Getal2;
+        }
+        public double Verschil()
+        {
+            return Getal1 - Getal2;
+        }
+        public double Product()
+        {
+            return Getal1 * Getal2;
+        }
+        public double Quotient()
+        {
+            return Getal1 / Getal2;
+        }
+    }
+}
---
+++ Classes/ClassesProgram.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Classes
+{
+    class ClassesProgram
+    {
+        static void Main(string[] args)
+        {
+            RapportModule MyRapportModule = new RapportModule();
+            MyRapportModule.percentage = 65;
+            MyRapportModule.PrintGraad();
+
+            Nummers paar1 = new Nummers();
+            paar1.Getal1 = 12;
+            paar1.Getal2 = 34;
+
+            Console.WriteLine("Paar:" + paar1.Getal1 + ", " + paar1.Getal2);
+            Console.WriteLine("Som = " + paar1.Som());
+            Console.WriteLine("Verschil = " + paar1.Verschil());
+            Console.WriteLine("Product = " + paar1.Product());
+            Console.WriteLine("Quotient = " + paar1.Quotient());
+        }
+    }
+}
