a0ec38bba10f295b92598a6b4726687152226188
wim forton <wim.forton@gmail.com>
2021-04-14 14:59:46 +0200

    Meetlat en Pokemon

---
+++ MemoryManagment/MemoryManagementProgram.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MemoryManagment
+{
+    class MemoryManagementProgram
+    {
+        static void Main(string[] args)
+        {
+            pokemon Bulbasaur = new pokemon();
+            Bulbasaur.Nummer = 1;
+            Bulbasaur.Naam = "Bulbasaur";
+            Bulbasaur.HP_Base = 45;
+            Bulbasaur.Attack_Base = 49;
+            Bulbasaur.Defense_Base = 49;
+            Bulbasaur.SpecialAttack_Base = 65;
+            Bulbasaur.SpecialDefense_Base = 65;
+            Bulbasaur.Speed_Base = 45;
+
+            pokemon Ivysaur = new pokemon();
+            Ivysaur.Nummer = 1;
+            Ivysaur.Naam = "Ivysaur";
+            Ivysaur.HP_Base = 45;
+            Ivysaur.Attack_Base = 49;
+            Ivysaur.Defense_Base = 49;
+            Ivysaur.SpecialAttack_Base = 65;
+            Ivysaur.SpecialDefense_Base = 65;
+            Ivysaur.Speed_Base = 45;
+
+            Console.WriteLine($"Naam = {Bulbasaur.Naam}\nAverage = {Bulbasaur.Average}\nTotal = {Bulbasaur.Total}");
+            for(int i = 0; i < 100; i++)
+            {
+                Bulbasaur.VerhoogLevel();
+                Bulbasaur.ShowInfo();
+
+            }
+        }
+    }
+}
